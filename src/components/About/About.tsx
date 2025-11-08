553727e84f54d3d88dbbd35afada0a547effbfef
Mohit Verma <mohitver01@gmail.com>
2025-11-08 15:24:59 -0500

    refactored code

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,24 +4,22 @@ import * as classes from './About.module.css';
 
 export function About() {
   return (
-    <Box>
-      <Flex className={classes.flexCont}>
-        <img className={classes.image} src="/static/img_About.jpg" />
+    <Flex className={classes.flexCont}>
+      <img className={classes.image} src="/static/img_About.jpg" />
 
-        <Box>
-          <Text className={classes.text}>
-            Mental health issues are a real concern for Canadians, which has roughly{' '}
-            <span>doubled</span> beween 2020 to 2023 (Statistics Canada).
-          </Text>
+      <Box>
+        <Text className={classes.text}>
+          Mental health issues are a real concern for Canadians, which has roughly{' '}
+          <span>doubled</span> beween 2020 to 2023 (Statistics Canada).
+        </Text>
 
-          <Text py={'20px'} className={classes.text}>
-            CalmVR's mission is to help Canadians overcome mental health struggles using virtual
-            reality. Our solution brings <span>you</span> to a <span>calm</span> and{' '}
-            <span>meditative</span> state by <span>engaging</span> your <span>hearing</span> and{' '}
-            <span>sight</span> through an immersive experience.
-          </Text>
-        </Box>
-      </Flex>
-    </Box>
+        <Text py={'20px'} className={classes.text}>
+          CalmVR's mission is to help Canadians overcome mental health struggles using virtual
+          reality. Our solution brings <span>you</span> to a <span>calm</span> and{' '}
+          <span>meditative</span> state by <span>engaging</span> your <span>hearing</span> and{' '}
+          <span>sight</span> through an immersive experience.
+        </Text>
+      </Box>
+    </Flex>
   );
 }
--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,23 +1,17 @@
-import React from "react";
-import { Box, Flex, Title, Text } from "@mantine/core";
-import * as classes from "./Welcome.module.css";
+import React from 'react';
+import { Box, Flex, Text, Title } from '@mantine/core';
+import * as classes from './Welcome.module.css';
 
 export function Welcome() {
   return (
-    <>
-      {/* <Box id="Landing" className={classes.main} style={{ height: 'calc(100vh - var(--nav-height) - var(--borderH))' }}> */}
-      <Flex id="Landing" className={classes.main}>
-        <Flex className={classes.content}>
-          <Title className={classes.title}>
-            <span>Calm</span><i>VR</i>
-          </Title>
-          <Text className={classes.text}>
-            Immerse Yourself Into Serenity
-          </Text>
-        </Flex>
+    <Flex id="Landing" className={classes.main}>
+      <Flex className={classes.content}>
+        <Title className={classes.title}>
+          <span>Calm</span>
+          <i>VR</i>
+        </Title>
+        <Text className={classes.text}>Immerse Yourself Into Serenity</Text>
       </Flex>
-
-
-    </>
+    </Flex>
   );
 }
